a973c32237c5ecfe1090eca93b72a86bfef3f60e
gyumining <ngm0123@naver.com>
2025-06-20 14:38:24 +0900

    Title: 점수 입출력, Time: 116ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250620/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner SC = new Scanner(System.in);
+        int age = SC.nextInt();
+        System.out.printf("Your score is %d point.",age);
+    }
+}
